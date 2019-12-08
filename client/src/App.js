03d3607e08d4d13756e615b1155573753bbfa8ff
GraceBenson <gracebenson@ufl.edu>
2019-12-08 14:06:54 -0500

    can insert to database, TODO: fix server crashes after though

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component{
   state = {
-    flowers: []
+    flowers: [],
+    sighting: {
+      name: '',
+      person: '',
+      location: '',
+      sighted: ''
+    }
   }
 
 
@@ -20,18 +25,33 @@ getFlowers = _ =>{
   .catch(err => console.error(err))
 }
 
-renderFlower(flower)
-{
- return(<div><button>{flower}</button></div>)
+addFlowers = _ =>{
+  const { sighting } = this.state
+  fetch(`http://localhost:4000/flowers/add?name=${sighting.name}&person=${sighting.person}&location=${sighting.location}&sighted=${sighting.sighted}`)
+  //.then(response => response.json())
+  .then(this.getFlowers)
+  .catch(err => console.error(err))
+}
+
+renderFlower(flower) {
+  return(<div><button>{flower}</button></div>)
 }
 
 
 render()
 {
-
+  const { sighting } = this.state
   return(
     <div className = "App">
       {this.state.flowers.map(flower=>this.renderFlower(flower))}
+
+      <div>
+        <input value={sighting.name} placeholder="Common Name" onChange={e => this.setState({ sighting: { ...sighting, name: e.target.value } })} />
+        <input value={sighting.person} placeholder="Person" onChange={e => this.setState({ sighting: { ...sighting, person: e.target.value } })} />
+        <input value={sighting.location} placeholder="Location" onChange={e => this.setState({ sighting: { ...sighting, location: e.target.value } })} />
+        <input value={sighting.sighted} placeholder="Sighted" onChange={e => this.setState({ sighting: { ...sighting, sighted: e.target.value } })} />
+        <button onClick={this.addFlowers}>Add</button>
+      </div>
     </div>
   )
 
