11e6a2d245dd023c2f600abc0c18a98b17ec1949
Sneha P <snehapatel@ufl.edu>
2019-12-09 21:36:59 -0500

    displays top 10 sightings for the flower button clicked

--- server/routes/index.js
+++ server/routes/index.js
@@ -37,16 +37,20 @@ app.get('/flowers', (req, res)=> {
 })
 
 //get top ten sightings for a flower
-app.get('/flowers/sightings', (req, res) =>
+app.get('/sightings', (req, res) =>
 {
   const {comname} = req.query;
   db.all('SELECT * FROM sightings WHERE name = ? LIMIT 10', [comname], (err, rows) => {
     if (err) {
       return console.error(err.message);
     }
-    console.log(rows)
-    res.json(rows);
-    return;   
+    else{
+      console.log(rows)
+    return res.json(rows);
+  
+
+    }
+    
    
   });
  
